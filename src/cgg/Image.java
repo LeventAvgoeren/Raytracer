1396befa8ed4ad7efdf20409b30387addf46f244
Levent Avgören <levent_avgoren@MacBook-Air-von-Levent.local>
2023-04-19 12:22:34 +0200

    Komplexes muster

--- src/cgg/Image.java
+++ src/cgg/Image.java
@@ -4,19 +4,28 @@ package cgg;
 import cgtools.*;
 
 public class Image {
+
+  double [] array;
+  int width;
+  int height;
+
   public Image(int width, int height) {
-    notYetImplemented();
+    this.width=width;
+    this.height=height;
+    this.array= new double[width*height*3];
   }
 
   public void setPixel(int x, int y, Color color) {
-    notYetImplemented();
+    //zu weisung der pixel 
+  int i=(y*width+x)*3;
+  array[i+0]=color.r();
+  array[i+1]=color.g();
+  array[i+2]=color.b();
   }
 
   public void write(String filename) {
-    // Use cggtools.ImageWriter.write() to implement this.
-    notYetImplemented();
+    ImageWriter.write(filename,array,width,height);
   }
-
   private void notYetImplemented() {
     System.err.println("Please complete the implementation of class cgg.Image as part of assignment 1.");
     System.exit(1);
--- src/cgg/a01/Main.java
+++ src/cgg/a01/Main.java
@@ -1,6 +1,7 @@
-/** @author henrik.tramberend@beuth-hochschule.de */
 package cgg.a01;
 
+import cgg.Image;
+import cgtools.Color;
 import static cgtools.Vector.*;
 import cgg.*;
 
@@ -11,19 +12,20 @@ public class Main {
     final int height = 270;
 
     // This class instance defines the contents of the image.
-    ConstantColor content = new ConstantColor(gray);
+    // bei 0,0,0 sieht man nichts 
+    KomplexMuster muster=new KomplexMuster(10);
 
     // Creates an image and iterates over all pixel positions inside the image.
     Image image = new Image(width, height);
     for (int x = 0; x != width; x++) {
       for (int y = 0; y != height; y++) {
         // Sets the color for one particular pixel.
-        image.setPixel(x, y, content.getColor(x, y));
+        image.setPixel(x, y, muster.getColor(x,y,red,black));
       }
     }
 
     // Write the image to disk.
-    final String filename = "doc/a01-image.png";
+    final String filename = "doc/a01-pattern.png";
     image.write(filename);
     System.out.println("Wrote image: " + filename);
   }
