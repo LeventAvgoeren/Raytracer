eda5582441068b1ce265db2ad22c7511247d997d
Levent Avgören <levent_avgoren@MacBook-Air-von-Levent.local>
2023-04-26 13:37:50 +0200

    Fertiges Projekt

--- src/cgg/a03/Ray.java
+++ src/cgg/a03/Ray.java
@@ -1,20 +1,29 @@
 package cgg.a03;
 import cgg.Image;
-import cgg.Image;
 import cgg.a02.Disc;
 import cgtools.Color;
+import cgtools.Vector;
+
 import static cgtools.Vector.*;
 import cgg.*;
 
 public class Ray {
 
-    public Ray(double tMin,double tMax,){
-
-    }
-
-
-    public void pointAt(double t ){
-
-    }
+        Vector origin;
+        Vector direction;
+        double tMin;
+        double tMax;
     
-}
+        public Ray(Vector origin, Vector direction, double tMin, double tMax) {
+            this.origin = origin;
+            this.direction = direction;
+            this.tMin = tMin;
+            this.tMax = tMax;
+        }
+    
+        public Vector pointAt(double t) {
+            return add(origin, multiply(t, direction));
+        }
+    
+    }
+
